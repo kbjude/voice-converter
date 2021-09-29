828111a3a13c62e5668895899202137eb10edf5d
kbjude <kbjude@gmail.com>
2021-09-30 00:23:28 +0300

    Add first code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace voiceconverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
