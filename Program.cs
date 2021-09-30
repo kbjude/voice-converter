860e34b8554e916fc2412b41e41771c0ea73bf93
kbjude <kbjude@gmail.com>
2021-09-30 14:41:07 +0300

    Refactor the code picking the files

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace voiceconverter
 {
@@ -7,15 +10,16 @@ namespace voiceconverter
     {
         static void Main(string[] args)
         {
-            
+
             //- Iterate through the subfolders of /mtn/uploads
-            foreach(var subfolder in Directory.GetDirectories("/mnt/uploads"))
+            foreach (var subfolder in Directory.GetDirectories("/Users/apple/www/projects/innovationvillage/learning/dotnet/uploads"))
             {
                 //- get the path to metadata file
 
                 var metadataFilePath = Path.Combine(subfolder, "metadata.json");
                 Console.WriteLine($"Reading {metadataFilePath}");
                 //- extract metadata, including audio file info from metadata file
+                var metadataCollection = GetMetada(metadataFilePath);
 
                 //* For each audio
                 //- Get the absolute path.
@@ -24,8 +28,18 @@ namespace voiceconverter
                 //- compress it
                 //- create its standalon meta file
             }
+        }
 
-            Console.WriteLine("Hello World!");
+
+        static List<Metadata> GetMetada(string metadataFilePath)
+        {
+            var metadataFileStream = File.Open(metadataFilePath, FileMode.Open);
+            var settings = new DataContractJsonSerializerSettings
+            {
+                DateTimeFormat = new DateTimeFormat("yyy-MM-dd'T'HH:mm:ssZ")
+            };
+            var serializer = new DataContractJsonSerializer(typeof(List<Metadata>), settings);
+            return  (List<Metadata>)serializer.ReadObject(metadataFileStream);
         }
     }
 }
